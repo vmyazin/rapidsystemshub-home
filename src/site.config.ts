7885736da3eed74a35a1c627022b6ff11678430a
Vasily Myazin <vm@konspiredesign.com>
2024-04-06 01:43:26 +0800

    Create site config

---
+++ src/site.config.ts
@@ -0,0 +1,9 @@
+type SiteConfig = {
+  image: string,
+  description: string
+}
+
+export const siteConfig: SiteConfig = {
+  image: "social.jpg",
+  description: "Digital marketing enterprise offering a tailored approach to lead generation and ad campaign management."
+};
