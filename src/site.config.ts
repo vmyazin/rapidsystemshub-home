11746cca93378cde7d257abda6abbd1c50abf32d
Vasily Myazin <vm@konspiredesign.com>
2024-04-07 14:32:49 +0800

    Update SEO variables

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,9 +1,11 @@
 type SiteConfig = {
+  sitename: string,
   image: string,
   description: string
 }
 
 export const siteConfig: SiteConfig = {
+  sitename: "Rapid Systems Hub",
   image: "social.jpg",
   description: "Digital marketing enterprise offering a tailored approach to lead generation and ad campaign management."
 };
